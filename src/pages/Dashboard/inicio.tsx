c8fcce664c3db4da3bb9f05c2f7ae198d924ac23
ccortes15 <47857246+ccortes15@users.noreply.github.com>
2020-11-10 13:30:20 -0600

    first commit

---
+++ src/pages/Dashboard/inicio.tsx
@@ -0,0 +1,8 @@
+import 'antd/dist/antd.css';
+import LayoutAdmin from '../../components/Layout/LayoutAdmin';
+
+export default function Home() {
+  return (
+      <LayoutAdmin />
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import 'antd/dist/antd.css';
+import Login from '../components/Login/LoginApp';
+
+export default function Home() {
+  return (
+      <Login />
+  )
+}
